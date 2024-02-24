3006e00d5fe2ac016be25233dbfb047dd0d2ca8e
Douglas Alisson <101756246+devdouglasa@users.noreply.github.com>
2024-02-24 08:19:40 -0300

    Add files via upload

---
+++ enviar_email.py
@@ -0,0 +1,40 @@
+import smtplib
+import email.message, requests
+from time import sleep
+
+cotacao_anterior = 4.9938
+
+url = 'https://economia.awesomeapi.com.br/last/USD-BRL'
+response = requests.get(url=url)
+response_json = response.json()
+cotacao_atual = float(response_json['USDBRL']['bid'])
+
+def enviar_email(cotacao: float):
+    corpo_email = f"""
+    <h1>COTAÇÃO DO DÓLAR</h1>
+
+    <h2>A cotação atual do dólar é {cotacao}</h2>
+    """
+
+    msg = email.message.Message()
+    msg['Subject'] = "TESTE SEND"
+    msg['From'] = 'douglasalisson27@gmail.com'
+    msg['To'] = 'douglasalisson27@gmail.com'
+    password = 'vjdwrygwfofaidmx'
+    msg.add_header('Content-Type', 'text/html')
+    msg.set_payload(corpo_email )
+
+    s = smtplib.SMTP('smtp.gmail.com: 587')
+    s.starttls()
+    # Login Credentials for sending the mail
+    s.login(msg['From'], password)
+    s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
+
+
+print('Initializing App...')
+
+if cotacao_atual != cotacao_anterior:
+    enviar_email(cotacao_atual)
+    cotacao_anterior = cotacao_atual
+    print(cotacao_atual)
+
